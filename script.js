9a23fbfc812ff37748f3efae9a9476f1b0473aba
Aingkaran <aingkaran1995@gmail.com>
2022-08-08 19:08:20 -0400

    Add HTML and styling of gameboard

---
+++ script.js
@@ -0,0 +1,30 @@
+const gameBoard = (()=>{
+
+    const gameBox= [1,2,3,
+                    4,5,6,
+                    7,8,9]
+    return {
+        gameBox
+    }
+
+})()
+
+
+
+const Player= (name,choice) =>{
+
+    const getName= name;
+
+    const getChoice = choice;
+
+    return (getName,getChoice)
+
+};
+
+
+const displayController =(()=>{
+
+    
+
+
+})()
